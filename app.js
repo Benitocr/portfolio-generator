1c642f22cb3962c9d022431a11efc3205abcdb51
Benito <benitocavazosr@yahoo.com>
2022-01-06 01:11:06 -0600

    ading array of projects to the user can save more than 1 project

--- app.js
+++ app.js
@@ -13,23 +13,90 @@ const inquirer = require('inquirer');
 // });
 
 
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-    {
-        type: 'input',
-        name:'name',
-        message: 'What is your name?'
-    }
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    console.log(answers)
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+const promptUser = () =>{
+    return inquirer.prompt([
+            /* Pass your questions in here */
+            {
+                type: 'input',
+                name:'name',
+                message: 'What is your name?'
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Enter your GitHub Username'
+            },
+            {
+                type: 'input',
+                name:'about',
+                message: 'Provide some information about yourself:'
+            }
+        ]);
+};
+
+
+
+const promptProject = portfolioData => {
+    
+    //If there is no projects array property, create one
+    if(!portfolioData.projects){
+        portfolioData.projects = [];
+    };
+    
+    console.log(`
+        =================
+        Add a New Project
+        =================
+    `);
+    return inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is the name of your project?'
+        },
+        {
+          type: 'input',
+          name: 'description',
+          message: 'Provide a description of the project (Required)'
+        },
+        {
+          type: 'checkbox',
+          name: 'languages',
+          message: 'What did you build this project with? (Check all that apply)',
+          choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+          type: 'input',
+          name: 'link',
+          message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+          type: 'confirm',
+          name: 'feature',
+          message: 'Would you like to feature this project?',
+          default: false
+        },
+        {
+          type: 'confirm',
+          name: 'confirmAddProject',
+          message: 'Would you like to enter another project?',
+          default: false
+        }
+      ])
+      .then(projectData => {
+          portfolioData.projects.push(projectData);
+          if(projectData.confirmAddProject){
+              return promptProject(portfolioData);
+            } else {
+                return portfolioData;
+            }
+      });
+    };
+
+promptUser()
+    // .then(answears => console.log(answears))
+    .then(promptProject)
+    // .then(projectAnswears => console.log(projectAnswears));
+    .then(portfolioData => {
+        console.log(portfolioData);
+    });

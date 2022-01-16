b0705f021725a68125f605d85e7d53fc2cc340c1
Benito <benitocavazosr@yahoo.com>
2022-01-15 22:06:06 -0600

    adding the code to generate index.html with diferent projects

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # portfolio-generator
+up to 9.4
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 // const pageHTML = generatePage(name, github);
 
 
@@ -151,5 +151,11 @@ promptUser()
     .then(promptProject)
     // .then(projectAnswears => console.log(projectAnswears));
     .then(portfolioData => {
-        console.log(portfolioData);
+        // console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err=>{
+            if(err) throw new Error(err);
+            console.log('Page created! Check out index.html in this directory to see it!');
+        });
     });
